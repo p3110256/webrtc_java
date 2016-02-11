2761ef9aa0e9ceb715bf3ec1b17309c93a0c9e49
redangel1993 <Kamperis1993@hotmail.com>
2016-02-11 15:11:10 +0200

    Disabled VPN alert

--- web/js/torrentsTimeFunctionality.js
+++ web/js/torrentsTimeFunctionality.js
@@ -19,8 +19,7 @@ $(document).ready(function () {
 
             var video_element = '<script>torrentsTime.init()</script>' +
                 '<div class="torrentsTime" id="torrentsTime"' +
-                'data-setup=\'{"source": "' +
-                input + '"}\'></div>';
+                'data-setup=\'{"source": "' +input + '","vpnAlert": false }\'></div>';
 
                 // hardcoded magnet backup
                 //'magnet:?xt=urn:btih:73A3E1F9492BA6B0FA5AFB95FEEBA59C001ABF62&dn=the+flash+2014+s02e13+hdtv+x264+lol+ettv&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce' +
